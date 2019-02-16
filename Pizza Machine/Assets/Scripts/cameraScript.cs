e4cd9dc5c4d726e882c44eeaf5be7498b38be7ad
beasterbro <43251102+beasterbro@users.noreply.github.com>
2019-02-16 14:42:49 -0500

    Made Camera Great Again
    Added x translation along with the zoom function that keeps both players on screen

--- Pizza Machine/Assets/Scripts/cameraScript.cs
+++ Pizza Machine/Assets/Scripts/cameraScript.cs
@@ -8,7 +8,7 @@ public class cameraScript : MonoBehaviour
     public GameObject player2;
     private Vector3 lastPlayerPosition1;
     private float distanceToMove1;
-    private Vector3 lastPlayerposition;
+    private Vector3 lastPlayerPosition2;
     private float distanceToMove2;
     public Camera camera;
 
@@ -16,21 +16,31 @@ public class cameraScript : MonoBehaviour
     void Start()
     {
         lastPlayerPosition1 = player1.transform.position;
-       // lastPlayerPosition2 = player2.transform.position;
+        lastPlayerPosition2 = player2.transform.position;
     }
 
     // Update is called once per frame
     void Update()
-    {//12 and 25 are arbitrary values where the camera is in a good spot
-        if(!(playerDist() <= 5) /*|| !(playerDist() >= 25)*/)
-             FixedCameraFollowSmooth(camera, player1.transform, player2.transform);
+    {
+        distanceToMove1 = player1.transform.position.x - lastPlayerPosition1.x;
+        distanceToMove2 = player2.transform.position.x - lastPlayerPosition2.x;
+        //5 is an arbitrary value where the camera is in a good spot
+        if (!(playerDist() <= 5) /*|| !(playerDist() >= 25)*/)
+            FixedCameraFollowSmooth(camera, player1.transform, player2.transform);
+        //Just move the camera normally
+        else
+        {
+            camera.transform.position = new Vector3(camera.transform.position.x + ((distanceToMove1 + distanceToMove2) / 2), camera.transform.position.y, camera.transform.position.z);
+        }
+        lastPlayerPosition1 = player1.transform.position;
+        lastPlayerPosition2 = player2.transform.position;
     }
 
     // Follow Two Transforms with a Fixed-Orientation Camera
     public void FixedCameraFollowSmooth(Camera cam, Transform t1, Transform t2)
     {
         // How many units should we keep from the players
-        float zoomFactor = 2f;
+        float zoomFactor = 1.8f;
         float followTimeDelta = 0.8f;
 
         // Midpoint we're after
@@ -48,6 +58,8 @@ public class cameraScript : MonoBehaviour
             // The camera's forward vector is irrelevant, only this size will matter
             cam.orthographicSize = distance;
         }
+
+        cam.transform.position = new Vector3(cam.transform.position.x + ((player1.transform.position.x + player2.transform.position.x) / 2), cam.transform.position.y, cam.transform.position.z);
         // You specified to use MoveTowards instead of Slerp
         cam.transform.position = Vector3.Slerp(cam.transform.position, cameraDestination, followTimeDelta);
 
