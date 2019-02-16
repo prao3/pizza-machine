3680c102b3d9151ffc7233f80dfae870bbd6ab56
prao3 <44528013+prao3@users.noreply.github.com>
2019-02-16 13:37:29 -0500

    Messing with some moving platforms

---
+++ Pizza Machine/Assets/Scripts/movingPlatform.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movingPlatform : MonoBehaviour
+{
+
+    public float xspeed = 1.0f;
+    public float yspeed = 1.0f;
+    public float oscTime = 1.0f;
+    private bool flip = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Time.deltaTime % oscTime == 0)
+            flip = !flip;
+
+        if (flip)
+            transform.position = transform.position + new Vector3(xspeed, yspeed, 0);
+
+        else
+            transform.position = transform.position + new Vector3(-xspeed, -yspeed, 0);
+
+    }
+}
