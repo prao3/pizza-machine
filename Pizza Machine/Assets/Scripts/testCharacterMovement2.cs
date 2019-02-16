e5ecaffb75a0753264951139e87132a556a50969
prao3 <44528013+prao3@users.noreply.github.com>
2019-02-16 10:55:32 -0500

    Created a player 2

--- Pizza Machine/Assets/Scripts/testCharacterMovement2.cs
+++ Pizza Machine/Assets/Scripts/testCharacterMovement2.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class testCharacterMovement : MonoBehaviour {
+public class testCharacterMovement2 : MonoBehaviour {
 
     //The rigid body of the player object
     private Rigidbody2D playerRigidBody2D;
@@ -16,29 +16,31 @@ public class testCharacterMovement : MonoBehaviour {
     //Checks if the player is on the ground
     private bool grounded = true;
 
-	// Use this for initialization
-	void Start () {
+    // Start is called before the first frame update
+    void Start()
+    {
 
         //Getting the rigid body of the player
         playerRigidBody2D = (Rigidbody2D)GetComponent(typeof(Rigidbody2D));
-		
-	}
-	
-	// Update is called once per frame hopefully
-	void Update () {
-        
-        if (Input.GetKey(KeyCode.RightArrow))
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.D))
             playerRigidBody2D.velocity = new Vector2(speed, playerRigidBody2D.velocity.y);
 
-        else if (Input.GetKey(KeyCode.LeftArrow))
+        else if (Input.GetKey(KeyCode.A))
             playerRigidBody2D.velocity = new Vector2(-speed, playerRigidBody2D.velocity.y);
-        
+
 
         //Checking if the player object can jump
-        if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded())
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded())
             jump();
-		
-	}
+
+    }
 
     //Returns a value indicating if the player object is on the ground or not
     public bool isGrounded()
