7520672824f96e9c79d06f20fdc84e281b968ad7
beasterbro <43251102+beasterbro@users.noreply.github.com>
2019-02-16 13:24:12 -0500

    Added a cameraScript
    Follows player 1 (Orange) only in the x direction

---
+++ Pizza Machine/Assets/Scripts/cameraScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraScript : MonoBehaviour
+{
+
+    public GameObject player1;
+    public GameObject player2;
+    private Vector3 lastPlayerPosition;
+    private float distanceToMove;
+
+    // Use this for initialization
+    void Start()
+    {
+        lastPlayerPosition = player1.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceToMove = player1.transform.position.x - lastPlayerPosition.x;
+
+        transform.position = new Vector3(transform.position.x + distanceToMove, transform.position.y , transform.position.z);
+
+        lastPlayerPosition = player1.transform.position;
+    }
+}
